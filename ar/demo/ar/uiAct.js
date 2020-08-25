107b046846e1357b3c94df214d597310ed145a9c
Geon-Hong Kim <geonhong.kim@ninanobiz.com>
2020-08-25 10:28:37 +0900

    2020-08-25 save version

--- ar/demo/ar/uiAct.js
+++ ar/demo/ar/uiAct.js
@@ -5,63 +5,62 @@ var layD = document.querySelector('.layD');
 var layE = document.querySelector('.layE');
 
 layA.style.zIndex = '17';
-layA.style.opacity = '1';
 
 document.querySelector('.layB .dirBtn').addEventListener('click', function(){
+    layB.style.display = 'none';
     layB.style.zIndex = '14';
-    layB.style.opacity = '0';
+    layA.style.display = 'flex';
     layA.style.zIndex = '17';
-    layA.style.opacity = '1';
 });
 
 document.querySelector('.layC .dirBtn').addEventListener('click', function(){
+    layC.style.display = 'none';
     layC.style.zIndex = '14';
-    layC.style.opacity = '0';
+    layB.style.display = 'flex';
     layB.style.zIndex = '17';
-    layB.style.opacity = '1';
 });
 
 document.querySelector('.layE .dirBtn').addEventListener('click', glassStart);
 document.querySelector('.layE .dirBtn').addEventListener('click', function(){
+    layE.style.display = 'none';
     layE.style.zIndex = '14';
-    layE.style.opacity = '0';
+    layA.style.display = 'flex';
     layA.style.zIndex = '17';
-    layA.style.opacity = '1';
 });
 
 document.querySelector('.layA .btn2').addEventListener('click', function(){
+    layA.style.display = 'none';
     layA.style.zIndex = '14';
-    layA.style.opacity = '0';
+    layE.style.display = 'flex';
     layE.style.zIndex = '17';
-    layE.style.opacity = '1';
     sts = true;
     console.log(sts);
 })
 
 document.querySelector('.layA .btn3').addEventListener('click', function(){
+    layA.style.display = 'none';
     layA.style.zIndex = '14';
-    layA.style.opacity = '0';
+    layB.style.display = 'flex';
     layB.style.zIndex = '17';
-    layB.style.opacity = '1';
 })
 
 document.querySelector('.layB .btn1').addEventListener('click', function(){
+    layB.style.display = 'none';
     layB.style.zIndex = '14';
-    layB.style.opacity = '0';
+    layC.style.display = 'flex';
     layC.style.zIndex = '17';
-    layC.style.opacity = '1';
 })
 
 document.querySelector('.layB .btn2').addEventListener('click', function(){
+    layB.style.display = 'none';
     layB.style.zIndex = '14';
-    layB.style.opacity = '0';
+    layC.style.display = 'flex';
     layC.style.zIndex = '17';
-    layC.style.opacity = '1';
 })
 
 document.querySelector('.layB .btn3').addEventListener('click', function(){
+    layB.style.display = 'none';
     layB.style.zIndex = '14';
-    layB.style.opacity = '0';
+    layC.style.display = 'flex';
     layC.style.zIndex = '17';
-    layC.style.opacity = '1';
 })
